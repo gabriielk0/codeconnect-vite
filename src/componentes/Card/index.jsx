13148c9613e2981a2c7990f13ca66e82f88f7dbd
Gabriel <oliveiracgabriel052@gmail.com>
2024-05-01 15:44:25 -0300

    estilizando o Card

--- src/componentes/Card/index.jsx
+++ src/componentes/Card/index.jsx
@@ -3,41 +3,43 @@ import Code from './assets/code.svg';
 import Chat from './assets/chat.svg';
 import Share from './assets/share.svg';
 import Icone from './assets/icone.png'
+import './styles.css'
 
 export default function Card() {
     return(
-        <article className="card">
-            <div className="card__imagem">
-                <img src={Capa} alt='imagem do post' />
-            </div>
-            <div className='card__conteudo'>
-                <div className='conteudo__texto'>
-                    <h3>Titudo do post</h3>
-                    <p>Resumo do post</p>
+        
+            <article className="card">
+                <div className="card__imagem">
+                    <img src={Capa} alt='imagem do post' />
                 </div>
+                <div className='card__conteudo'>
+                    <div className='conteudo__texto'>
+                        <h3>Titudo do post</h3>
+                        <p>Resumo do post</p>
+                    </div>
 
-                <div className='conteudo__rodape'>
-                    <ul>
-                        <li>
-                            <img src={Code} alt='códigos'/>
-                            100
-                        </li>
-                        <li>
-                            <img src={Share} alt='compartilhamentos'/>
-                            12
-                        </li>
-                        <li>
-                            <img src={Chat} alt='comentários'/>
-                            10
-                        </li>
-                    </ul>
+                    <div className='conteudo__rodape'>
+                        <ul>
+                            <li>
+                                <img src={Code} alt='códigos'/>
+                                100
+                            </li>
+                            <li>
+                                <img src={Share} alt='compartilhamentos'/>
+                                12
+                            </li>
+                            <li>
+                                <img src={Chat} alt='comentários'/>
+                                10
+                            </li>
+                        </ul>
 
-                    <div className='roda__usuario'>
-                        <img src={Icone} alt='Imagem do Usuário'/>
-                        @Gabriel
+                        <div className='rodape__usuario'>
+                            <img src={Icone} alt='Imagem do Usuário'/>
+                            @Gabriel
+                        </div>
                     </div>
                 </div>
-            </div>
-        </article>
+            </article>
     )
 }
