50816960a4e3a7f013586be74ce24c16764103b9
Gabriel <oliveiracgabriel052@gmail.com>
2024-05-01 13:17:16 -0300

    adicionado estilos ao sidebar

--- src/componentes/Sidebar/index.jsx
+++ src/componentes/Sidebar/index.jsx
@@ -3,36 +3,37 @@ import Feed from './assets/feed.svg';
 import Account from './assets/account_circle.svg';
 import Info from './assets/info.svg';
 import Logout from './assets/info.svg';
+import './styles.css';
 
 export default function Sidebar() {
     return (
         <aside>
             <img src={Logo} alt='Logo do CodeConnect' />
             <nav>
-                <ul>
+                <ul className='lista-sidebar'>
                     <li>
-                        <a href='#'>Publicar</a>
+                        <a href='#' className='item__link--publicacao'>Publicar</a>
                     </li>
                     <li>
-                        <a href='#'>
+                        <a href='#' className='item__link item__link--ativo'>
                             <img src={Feed} alt='' />
                             <span>Feed</span>
                         </a>
                     </li>
                     <li>
-                        <a href='#'>
+                        <a href='#' className='item__link'>
                             <img src={Account} alt='' />
                             <span>Perfil</span>
                         </a>
                     </li>
                     <li>
-                        <a href='#'>
+                        <a href='#' className='item__link'>
                             <img src={Info} alt='' />
                             <span>Sobre Nós</span>
                         </a>
                     </li>
                     <li>
-                        <a href='#'>
+                        <a href='#' className='item__link'>
                             <img src={Logout} alt='' />
                             <span>Sair</span>
                         </a>
